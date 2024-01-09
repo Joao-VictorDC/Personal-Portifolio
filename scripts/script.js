bc68ea4e455bdb596292ead32e4545a81171913c
Joao Victor <dorea.cupolo@gmail.com>
2024-01-09 12:34:09 -0300

    Adicionando seções

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-let tablinks = document.getElementsByClassName("tab-links"); 
+let tablinks = document.getElementsByClassName("tab-links"); //cria variável let (não pode ser usada novamente)
 let tabcontents = document.getElementsByClassName("tab-contents");
 
 
-function opentab(tabname) {
-    for(tablink of tablinks){
+function opentab(tabname) { //estrutura de uma função, com um parâmetro 'tabname'
+    for(tablink of tablinks){ //laço for - of (ver docs)
         tablink.classList.remove("active-link");
     }
     for(tabcontent of tabcontents){
         tabcontent.classList.remove("active-tab");
     }
-    event.currentTarget.classList.add("active-link");
+    event.currentTarget.classList.add("active-link"); //adiciona um evento (ver docs)
     document.getElementById(tabname).classList.add("active-tab");
 }
